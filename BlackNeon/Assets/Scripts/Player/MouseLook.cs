a9181700fef27d34fbe0e2e802c7f2b8f12b9d73
Helioce <88891978+I-Helioce-I@users.noreply.github.com>
2023-09-01 11:02:15 +0200

    Reorganised scripts folder, Add new player controller need to be tested and approved

--- BlackNeon/Assets/Scripts/Player/MouseLook.cs
+++ BlackNeon/Assets/Scripts/Player/MouseLook.cs
@@ -9,10 +9,23 @@ public class MouseLook : MonoBehaviour
 
     public Transform playerBody;
 
+    float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     private void Update()
     {
         float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
