3f58f9d52f2f01011df1a78a1ca9d14bd3bc7131
Alex <fewfwef@fwef.com>
2023-09-09 17:46:16 +0200

    ADD LeaderboardEntry.cs and sorting method

---
+++ BlackNeon/Assets/Scripts/PlayFab/LeaderboardExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public static class LeaderboardExtension
+{
+	public static void DebugLeaderboard(this List<LeaderboardEntry> entryList)
+	{
+		StringBuilder stringBuilder = new StringBuilder();
+
+		foreach (var item in entryList)
+		{
+			stringBuilder.AppendLine(item.ToString());
+		}
+
+		Debug.Log(stringBuilder.ToString());
+	}
+}
