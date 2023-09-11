5cbeeba1756c9319db5e2587d643d57e6c3700d5
Helioce <88891978+I-Helioce-I@users.noreply.github.com>
2023-09-11 17:34:50 +0200

    ADD ChargesGiver VFX

--- BlackNeon/Assets/Scripts/InGame/AddCharge.cs
+++ BlackNeon/Assets/Scripts/InGame/AddCharge.cs
@@ -1,11 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class AddCharge : MonoBehaviour
 {
     [SerializeField]
     chargeType charge;
+
+    [SerializeField]
+    Material chargeColor;
+
+    [SerializeField]
+    Color[] colors;
+
+    private void Start()
+    {
+        chargeColor = GetComponent<MeshRenderer>().material;
+
+        switch (charge)
+        {
+            case chargeType.Add:
+                chargeColor.SetColor("_EmissionColor", colors[0] * Mathf.Pow(2.0F, 4));
+                break;
+            case chargeType.Substract:
+                chargeColor.SetColor("_EmissionColor", colors[1] * Mathf.Pow(2.0F, 4));
+                break;
+            case chargeType.Multiply:
+                chargeColor.SetColor("_EmissionColor", colors[2] * Mathf.Pow(2.0F, 4));
+                break;
+            case chargeType.Divide:
+                chargeColor.SetColor("_EmissionColor", colors[3] * Mathf.Pow(2.0F, 4));
+                break;
+            default:
+                break;
+        }
+
+
+    }
 
     private void OnTriggerEnter(Collider other)
     {
